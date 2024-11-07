ef8aca7b8afc6475d5d43e2d7d7fd0dd51f3415e
Oleksii Chmil <66308922+oleksiichmil@users.noreply.github.com>
2024-11-07 19:09:19 +0200

    change

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+      refs.modal.classList.toggle("is-open");
+    }
+  })();
